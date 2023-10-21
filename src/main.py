794d1c8c8b7ffe50e77e652d377b7602683d82e6
fedi-nabli <fedinabli@gmail.com>
2023-10-21 16:20:30 +0100

    Created sheets module

--- src/main.py
+++ src/main.py
@@ -1,10 +1,14 @@
 from modules.auth_module import AuthModule
 from modules.drive_module import DriveModule
+from modules.sheets_module import SheetsModule
 
 def main():
   auth_session = AuthModule()
   drive_instance = DriveModule(auth_session.creds)
-  print(drive_instance.list_files())
+  sheets_instance = SheetsModule(auth_session.creds)
+  files = drive_instance.list_files()
+  print(files)
+  print(sheets_instance.list_values(files[1].get('id')))
 
 if __name__ == '__main__':
   main()
---
+++ src/modules/sheets_module.py
@@ -0,0 +1,32 @@
+from __future__ import print_function
+from google.oauth2.credentials import Credentials
+from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
+
+class SheetsModule():
+  def __init__(self, creds: Credentials = None) -> None:
+    try:
+      self.services = build('sheets', 'v4', credentials=creds)
+
+    except HttpError as error:
+      print(f'An error occured: {error}')
+  
+  def list_values(self, spreadhseet_id: str = None) -> list:
+    try:
+      rows = []
+
+      spreadhseet = self.services.spreadsheets().get(
+        spreadsheetId=spreadhseet_id
+      ).execute()
+      range = spreadhseet['sheets'][0]['properties']['title']
+
+      result = self.services.spreadsheets().values().get(
+        spreadsheetId=spreadhseet_id, range=range
+      ).execute()
+
+      rows.extend(result.get('values', []))
+      
+    except HttpError as error:
+      print(f'An error occured: {error}')
+
+    return rows
