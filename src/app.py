49230da8f3c81cd88f9ce7444f9b5c8f671884fb
fedi-nabli <fedinabli@gmail.com>
2023-12-30 18:46:07 +0100

    Added json in routes

--- src/app.py
+++ src/app.py
@@ -5,7 +5,7 @@ from modules.sheets_module import SheetsModule
 
 import database.api as db
 
-from utils.list_to_json import convert_list_to_dicts
+from utils.list_to_json import convert_list_to_dicts, convert_list_to_json
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
@@ -19,20 +19,30 @@ sheets_instance = SheetsModule(auth_session.creds)
 def index():
   return 'API is running'
 
-@app.route('/spreadsheets')
+@app.route('/spreadsheets/list')
 def list_spreadsheets():
   spreadhseets = drive_instance.list_files()
   return spreadhseets
+
+@app.route('/spreadsheets')
+def list_spreadsheets_json():
+  spreadhseets = drive_instance.list_files()
+  return convert_list_to_json(spreadhseets)
 
 @app.route('/spreadsheets/<spreadsheet_id>/no-filter')
 def list_spreadsheet_values_unfiltered(spreadsheet_id=None):
   rows = sheets_instance.list_values(spreadsheet_id)
   return rows
 
+@app.route('/spreadsheets/<spreadsheet_id>/list')
+def list_spreadsheet_values_list(spreadsheet_id=None):
+  rows = sheets_instance.list_values(spreadsheet_id)
+  return convert_list_to_dicts(rows)
+
 @app.route('/spreadsheets/<spreadsheet_id>')
 def list_spreadsheet_values(spreadsheet_id=None):
   rows = sheets_instance.list_values(spreadsheet_id)
-  return convert_list_to_dicts(rows)
+  return convert_list_to_json(convert_list_to_dicts(rows))
 
 if __name__ == '__main__':
   app.run(debug=True, port=15000)
--- src/utils/list_to_json.py
+++ src/utils/list_to_json.py
@@ -1,3 +1,5 @@
+import json
+
 def convert_list_to_dicts(spreadsheet_items: list[list[str]]) -> list[dict]:
   column_names: list[str] = spreadsheet_items[0]
   rows: list[list[str]] = spreadsheet_items[1:]
@@ -5,7 +7,7 @@ def convert_list_to_dicts(spreadsheet_items: list[list[str]]) -> list[dict]:
   json_list: list[dict] = []
 
   for row in rows:
-    row_dict = {}
+    row_dict: dict = {}
     for idx in range(len(column_names)):
       if column_names[idx]:
         row_dict[column_names[idx]] = row[idx]
@@ -15,3 +17,13 @@ def convert_list_to_dicts(spreadsheet_items: list[list[str]]) -> list[dict]:
       json_list.append(row_dict)
 
   return json_list
+
+def convert_list_to_json(dict_list: list[list[str]]):
+  count: int = len(dict_list)
+  data = {
+    "count": count,
+    "items": dict_list
+  }
+
+  json_data = json.dumps(data, separators=(",", ":"))
+  return json_data
